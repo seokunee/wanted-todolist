fd97cc4840ed96f2166caf7761b2e1640039861f
seokunee <xocoolree@gmail.com>
2022-08-08 02:20:53 +0900

    [feat] signup api 추가

--- frontend/src/components/SignUp/SignUp.tsx
+++ frontend/src/components/SignUp/SignUp.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
@@ -12,6 +13,24 @@ const SignUp = () => {
     const { value, name } = e.currentTarget;
     setInputs({ ...inputs, [name]: value });
   };
+
+  const createUser = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/users/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: inputs.email,
+          password: inputs.password,
+        }),
+      });
+      console.log("response = ", response);
+    } catch {
+      console.error("signup error!");
+    }
+  };
   const submit = () => {};
 
   return (
@@ -23,6 +42,7 @@ const SignUp = () => {
           name="email"
           className={styles.input}
           onChange={onChageInputs}
+          value={inputs.email}
           type="text"
         />
         <span className={styles.title}>비밀번호</span>
@@ -30,6 +50,7 @@ const SignUp = () => {
           name="password"
           className={styles.input}
           onChange={onChageInputs}
+          value={inputs.password}
           type="password"
         />
         <span className={styles.title}>비밀번호 확인</span>
@@ -37,12 +58,18 @@ const SignUp = () => {
           name="checkPassword"
           onChange={onChageInputs}
           className={styles.input}
+          value={inputs.checkPassword}
           type="password"
         />
       </div>
-      <button onClick={submit} className={styles.submit}>
-        완료
-      </button>
+      <div className={styles.submit_container}>
+        <Link to="/login">
+          <button className={styles.submit}>이전</button>
+        </Link>
+        <button onClick={createUser} className={styles.submit}>
+          완료
+        </button>
+      </div>
     </div>
   );
 };
